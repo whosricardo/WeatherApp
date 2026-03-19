07ca4d276371c7e77d4f32650b25887e469f591f
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-03-19 15:52:34 -0300

    fix: remove duplicate forecast locations

--- app/src/main/java/com/example/findinglogs/model/repo/Repository.java
+++ app/src/main/java/com/example/findinglogs/model/repo/Repository.java
@@ -42,8 +42,6 @@ public class Repository {
         localizations.put("1", "-8.05428,-34.8813");
         localizations.put("2", "-9.39416,-40.5096");
         localizations.put("3", "-8.284547,-35.969863");
-        localizations.put("4", "-8.284547,-35.969863");
-        localizations.put("5", "-9.39416,-40.5096");
         return localizations;
     }
 }
